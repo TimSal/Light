aec47c658a33b62a96838de5e4f2f916a6d6bab4
Тим <timsal@macbook-pro-tim.local>
2019-03-04 14:37:05 +0500

    Initial Commit

---
+++ Light/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Light
+//
+//  Created by Tim Sal on 04/03/2019.
+//  Copyright © 2019 Tim Sal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
